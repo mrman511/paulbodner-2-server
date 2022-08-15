04fd643790b6a3a980c9bbe15e80ca7c6423fc39
Paul Bodner <bodner80@gmail.com>
2022-08-15 10:44:55 -0700

    implemented routes for get projects

--- db/seeds/01_projects.sql
+++ db/seeds/01_projects.sql
@@ -1,20 +1,28 @@
-INSERT INTO projects (title, description, image_path_name) VALUES 
+INSERT INTO projects (title, description, image_path_name, link, github) VALUES 
 ('PaulBodner.com',
 'My Personal Portfolio, this page was made possible with the Fullpage.js 
 framework, and demonstrates my ability to work with apreviously unfamiliar 
 framework when educational resources are sparse and my ability to 
 understand the documentation and the code becomes paramount. This WebPage 
 was created using React, Fullpage.js, ReactTransitions, and and Express Api',
-'paulbodner-site'),
+'paulbodner-site',
+'https://paulbodner.com',
+'https://github.com/mrman511/pb-front-end-v2'),
+
 ('Dev Search', 
 'Created during Udemy course Python Django - The Complete Course. 
 This project many important Django features such as User Authentication, 
 Admin User, Password Recovery, User Profiles, User Owned Models, 
 Interacting with Models, Insite messaging. etc',
- 'devsearch'),
+'devsearch',
+'https://paul-devsearch.herokuapp.com/',
+'https://github.com/mrman511/learn-django'),
+
 ('Pill Popper',
 'A React front end app used for primary function of scheduling Medications, 
 with a side of education with links from your selected medication to the 
 FDA database. This app runs with an Express served API, as well as 
 Google Maps, Pusher notifications, and Twilio SMS.',
-'pillpopper');
+'pillpopper', 
+'https://nimble-starburst-c30e53.netlify.app/',
+'https://github.com/mrman511/pill-popper');
